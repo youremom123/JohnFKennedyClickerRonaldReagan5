82756e5a291e3cd0012ff111aaa528226b2e017a
xeriv54941 <xeriv54941@to200.com>
2022-03-31 19:40:43 +0200

    new game changes 213

---
+++ BatmanJohnReaganFortniteSimGiraf7/Assets/autobiden.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autobiden : MonoBehaviour
+{
+    public int moneySecond;
+    int itemAmount;
+
+    public float autoPrice;
+    float cooldownTimer = 1;
+    float currentTime;
+   
+    clickerbutton sørenScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sørenScript = FindObjectOfType<clickerbutton>();
+    }
+
+    // Update is called once per frame lolololololololol
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 0)
+        {
+            currentTime = cooldownTimer;
+            sørenScript.moneyAddition(moneySecond * itemAmount);
+        } 
+        
+    }
+    public void Karl()
+    {
+   
+    }
+    public void Svend()
+    {
+        if (sørenScript.amountOfMoney >= autoPrice)
+        {
+            sørenScript.amountOfMoney -= autoPrice;
+            itemAmount += 1;
+        }
+       
+    }
+}
---
+++ BatmanJohnReaganFortniteSimGiraf7/Assets/clickerbutton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class clickerbutton : MonoBehaviour
+{
+    public float amountOfMoney;
+    public float addMoney;
+    public Text moneyText;
+
+
+
+    // asoink bob, oleg lugter
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyText.text = amountOfMoney.ToString("F0");
+    }
+
+    public void anders()
+    {
+        amountOfMoney += addMoney;
+    }
+    public void  moneyAddition(float moneyToAdd)
+    {
+        amountOfMoney += moneyToAdd;
+
+    }
+}
